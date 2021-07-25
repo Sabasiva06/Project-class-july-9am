f76cbda5de0aee9dcb68a8543198055048bc99f0
Sabasiva06 <sabasiva06@gmail.com>
2021-07-25 14:43:29 +0530

    technology works

--- src/org/login/Employee.java
+++ src/org/login/Employee.java
@@ -2,36 +2,42 @@ package org.login;
 
 public class Employee {
 
-	
 	private void employeeId() {
 		System.out.println("employee id is 123");
 	}
-	
-	
+
 	private void employeeName() {
 		System.out.println("employee name is ravi");
 	}
-	
+
 	private void employeeAddress() {
 		System.out.println("employee address is omr");
 	}
-	
+
+	private void employeeDepartment() {
+		System.out.println("Mechanical Department");
+
+	}
+
+	private void employeeSalary() {
+		System.out.println("Employee Salary 10 lakhs");
+
+	}
+
+	private void employeeLocation() {
+		System.out.println("Employee Location : Chennai");
+
+	}
+
 	public static void main(String[] args) {
-		//Classname objname = new Classname();
+		// Classname objname = new Classname();
 		Employee a = new Employee();
-		
-		//objname.methodname
+
+		// objname.methodname
 		a.employeeId();
 		a.employeeName();
 		a.employeeAddress();
-		
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
